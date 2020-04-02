0d32ef527f9ddbbd93b68037d172cf9d23a978b6
Jackson-Zheng <zhengzhiyongzhengzhiyong@hotmail.com>
2020-04-02 17:04:09 +0800

    edited

---
+++ src/Taskfactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using FluentScheduler;
+
+  public class Taskfactory : Registry
+    {
+        public Taskfactory()
+        {
+            var url = ConfigurationManager.AppSettings["UrlToPing"].ToString();
+
+            Schedule<AllBackup>().ToRunNow().AndEvery(2).Seconds();
+        }
+    }
+
+    public class AllBackup:IJob
+    {
+        public void Execute()
+        {
+            Console.WriteLine("mysql backup");
+        }
+    }
+
+
---
+++ src/Worker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Configuration;
+using FluentScheduler;
+
+class Program
+{
+    public static bool running;
+
+    public static void Log(string s)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {s}");
+    }
+
+    public static void Loop()
+    {
+        Log("Started SampleWorker(c# version)");
+
+        //JobManager.Initialize(new Taskfactory());
+        //while (running)
+        //{
+        //    Log("Running");
+        //    var url = ConfigurationManager.AppSettings["UrlToPing"].ToString();
+        //    Log(url);
+        //    Thread.Sleep(2000);
+        //}
+        Log("Stopped SampleWorker(c# version)");
+    }
+
+    public static void Main()
+    {
+        //var th = new Thread(Loop);
+
+        //running = true;
+        //th.Start();
+
+        var msg
+            = Console.ReadLine();
+        Log($"Received message \"{msg}\" from the Monitor");
+
+        //running = false;
+        //th.Join();
+    }
+}
